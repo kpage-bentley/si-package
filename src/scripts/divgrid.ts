5e0408724bf4172bc9e21fd53864ce259738e48e
Christian Paul Dehli <cpdehli@gmail.com>
2016-04-12 22:45:51 -0400

    Finished reworking divgrid so it has all the existing functionality.

--- src/scripts/divgrid.ts
+++ src/scripts/divgrid.ts
@@ -5,65 +5,24 @@ var createParcoordsGrid = function (parcoords, gridElement, data) {
     // Total number of rows to display
     const GRID_ROWS = 50;
 
-    var sort = {
-        axis: null,
-        up: false
-    };
-
-    function repeatElement(dataArray: any[], elementConstructor, limit?: number): any[] {
-        limit = typeof limit === 'undefined' ? dataArray.length : limit;
-
-        var result = [];
-        for (var i = 0; i < Math.min(limit, dataArray.length); ++i) {
-            var data = dataArray[i];
-            var element = elementConstructor(data, i);
-            result.push(element);
-        }
-        return result;
-    }
-
-    function resetHeaderIcons(skipIndex) {
-        var theadRow = gridElement.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
-        var icons = theadRow.getElementsByTagName("i");
-        for (var i = 0; i < icons.length; ++i) {
-            if (i !== skipIndex) {
-                icons[i].className = "";
-            }
-        }
-    }
-
-    var dg: any = function () {
-
+    function createRows(sortOnColumn?: string, sortUp?: boolean) {
         var columns = d3.keys(data[0]);
 
-        // Create table skeleton
-        gridElement.innerHTML = "<table class='table table-hover'>" +
-                                    "<thead><tr></tr></thead>" +
-                                    "<tbody></tbody>" +
-                                "</table>";
-
-        // Populate table header
-        var theadRow = gridElement.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
-        var headColumns = repeatElement(columns, (col, idx) => {
-            var th = document.createElement("th");
-            th.innerHTML = col + "<i></i>";
-
-            th.addEventListener('mousedown', e => {
-                resetHeaderIcons(idx);
-                var icon = th.getElementsByTagName("i")[0];
-                var icons = ['', 'icon-chevron_down', 'icon-chevron_up'];
-                var index = (icons.indexOf(icon.className) + 1) % icons.length;
-                icon.className = icons[index];
+        // Create copy of data
+        var sorted = data.slice();
+        if (typeof sortOnColumn !== 'undefined') {
+            sorted.sort((a, b) => {
+                return a[sortOnColumn] - b[sortOnColumn]
             });
-            return th;
-        });
-        for (var i = 0; i < headColumns.length; ++i) {
-            theadRow.appendChild(headColumns[i]);
+            if (sortUp) {
+                sorted.reverse();
+            }
         }
 
         // Populate table body
         var tbody = gridElement.getElementsByTagName("tbody")[0];
-        var tableRows = repeatElement(data, d => {
+        tbody.innerHTML = "";
+        var tableRows = repeatElement(sorted, d => {
             var entries = columns.map(col => d[col]);
             var tr = document.createElement("tr");
 
@@ -86,15 +45,81 @@ var createParcoordsGrid = function (parcoords, gridElement, data) {
         for (var i = 0; i < tableRows.length; ++i) {
             tbody.appendChild(tableRows[i]);
         }
+    }
 
-        return dg;
+    function createTable() {
+
+        var columns = d3.keys(data[0]);
+
+        // Create table skeleton
+        gridElement.innerHTML = "<table class='table table-hover'>" +
+                                    "<thead><tr></tr></thead>" +
+                                    "<tbody></tbody>" +
+                                "</table>";
+
+        // Populate table header
+        var theadRow = gridElement.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
+        var headColumns = repeatElement(columns, col => {
+            var th = document.createElement("th");
+            th.innerHTML = col + "<i></i>";
+
+            th.addEventListener('mousedown', e => {
+                resetHeaderIcons(col);
+                var icon = th.getElementsByTagName("i")[0];
+                if (icon.className === '') {
+                    icon.className = 'icon-chevron_down';
+                    createRows(col, true);
+                }
+                else if (icon.className === 'icon-chevron_down') {
+                    icon.className = 'icon-chevron_up';
+                    createRows(col, false);
+                }
+                else {
+                    icon.className = '';
+                    createRows();
+                }
+            });
+            return th;
+        });
+        for (var i = 0; i < headColumns.length; ++i) {
+            theadRow.appendChild(headColumns[i]);
+        }
+
+        createRows();
+    }
+
+    function repeatElement(dataArray: any[], elementConstructor, limit?: number): any[] {
+        limit = typeof limit === 'undefined' ? dataArray.length : limit;
+
+        var result = [];
+        for (var i = 0; i < Math.min(limit, dataArray.length); ++i) {
+            var data = dataArray[i];
+            var element = elementConstructor(data);
+            result.push(element);
+        }
+        return result;
+    }
+
+    function resetHeaderIcons(skipValue?: any) {
+        var theadRow = gridElement.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
+        var icons = theadRow.getElementsByTagName("i");
+        for (var i = 0; i < icons.length; ++i) {
+            var icon = icons[i];
+            if (icon.previousSibling.textContent !== skipValue) {
+                icon.className = "";
+            }
+        }
+    }
+
+    function brush(elements) {
+        data = elements;
+        resetHeaderIcons();
+        createRows();
+    }
+
+    createTable();
+
+    return {
+        brush: brush
     };
-
-    dg();
-
-    dg.brush = function(elements) {
-        sort.axis = null;
-    };
-
-    return dg;
 };
